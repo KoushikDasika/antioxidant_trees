6ae027506382414abd3c40d4fed7709e484319f9
Koushik Dasika <koushikdasika@gmail.com>
2024-11-21 13:27:56 -0500

    successful html response

--- oxidized_trees/src/main.rs
+++ oxidized_trees/src/main.rs
@@ -1,24 +1,41 @@
 #[macro_use]
 extern crate rocket;
+extern crate rocket_dyn_templates;
 
 pub mod tree;
+use rocket::State;
+use rocket_dyn_templates::{context, Template};
 pub use tree::Node;
 pub use tree::Tree;
 
-#[get("/")]
+#[get("/hello")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/descendants/<id>")]
+fn descendants(tree: &State<Tree>, id: u32) -> Template {
+    let descendants = tree
+        .get_descendants(id)
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<Vec<String>>();
+    Template::render(
+        "descendants",
+        context! {
+            title: "Descendants",
+            descendants: descendants
+        },
+    )
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let tree = setup();
-    dbg!(&tree);
-    // dbg!(tree.common_ancestors(4, 5));
-    // dbg!(tree.common_ancestors(4, 7));
-
     let _rocket = rocket::build()
-        .mount("/hello", routes![index])
+        .mount("/", routes![index, descendants])
+        .manage(tree)
+        .attach(rocket_dyn_templates::Template::fairing())
         .launch()
         .await?;
 
@@ -27,14 +44,10 @@ async fn main() -> Result<(), rocket::Error> {
 
 fn setup() -> Tree {
     let mut tree = Tree::new();
-
     tree.add_node(1, None);
     tree.add_node(2, Some(1));
     tree.add_node(3, Some(1));
     tree.add_node(4, Some(2));
     tree.add_node(5, Some(2));
-    tree.add_node(6, Some(3));
-    tree.add_node(7, Some(3));
-
     tree
 }
