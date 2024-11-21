5cf3a53ff20585c44ff3b36b0d54f6746af7dc47
Koushik Dasika <koushikdasika@gmail.com>
2024-11-21 00:13:02 -0500

    added common ancestors

--- oxidized_trees/src/tree/node.rs
+++ oxidized_trees/src/tree/node.rs
@@ -2,19 +2,29 @@
 pub struct Node {
     pub id: u32,
     pub parent_id: Option<u32>,
-    pub path: String
+    pub path: String,
+    pub ancestors: Vec<u32>,
 }
 
 impl Node {
-    pub fn new(id: u32, parent_id: Option<u32>, path: String) -> Self {
+    pub fn new(id: u32, parent_id: Option<u32>, path: String, ancestors: Vec<u32>) -> Self {
         Node {
             id,
             parent_id,
-            path
+            path,
+            ancestors,
         }
     }
 
     pub fn get_parent_id(&self) -> Option<u32> {
         self.parent_id
+    }
+
+    pub fn get_path(&self) -> String {
+        self.path.clone()
+    }
+
+    pub fn get_ancestors(&self) -> Vec<u32> {
+        self.ancestors.clone()
     }
 }
--- oxidized_trees/src/tree/tree.rs
+++ oxidized_trees/src/tree/tree.rs
@@ -21,15 +21,58 @@ impl Tree {
             true => {
                 let parent_node = self.nodes.get(&parent_id.unwrap()).unwrap();
                 let new_path = format!("{}.{}", parent_node.path, id);
-                let new_node = Node::new(id, Some(parent_id.unwrap()), new_path);
+                let mut ancestors = parent_node.ancestors.clone();
+                ancestors.push(parent_node.id);
+
+                let new_node = Node::new(id, Some(parent_id.unwrap()), new_path, ancestors);
                 self.nodes.insert(id, new_node);
             }
             false => {
-                let new_node = Node::new(id, None, String::from(id.to_string()));
+                let new_node = Node::new(id, None, String::from(id.to_string()), Vec::new());
                 self.nodes.insert(id, new_node);
             }
         };
 
         self.count += 1;
+    }
+
+    pub fn common_ancestors(&self, id1: u32, id2: u32) -> Vec<u32> {
+        let node1 = self.nodes.get(&id1).unwrap();
+        let node2 = self.nodes.get(&id2).unwrap();
+
+        dbg!(&node1.get_ancestors());
+        dbg!(&node2.get_ancestors());
+
+        let output = node1
+            .get_ancestors()
+            .iter()
+            .filter(|a| node2.get_ancestors().contains(a))
+            .copied()
+            .collect();
+
+        dbg!(&output);
+
+        output
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_common_ancestors() {
+        let mut tree = Tree::new();
+
+        tree.add_node(1, None);
+        tree.add_node(2, Some(1));
+        tree.add_node(3, Some(1));
+        tree.add_node(4, Some(2));
+        tree.add_node(5, Some(2));
+        tree.add_node(6, Some(3));
+
+        assert_eq!(tree.common_ancestors(4, 5), vec![1, 2]);
+        assert_eq!(tree.common_ancestors(4, 6), vec![1]);
+        assert_eq!(tree.common_ancestors(1, 6), vec![]);
     }
 }
